e7a946f3e26983047021a814f6f8c6b7f91bc6fd
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2021-02-03 17:09:19 -0300

    projeto01 Quase pronto

--- dom/6 - Projeto01/app.js
+++ dom/6 - Projeto01/app.js
@@ -4,41 +4,83 @@ new Vue({
         vidaJ: 100,
         vidaM: 100,
         starter: false,
+        porcentagemJ: '',
+        porcentagemM: '',
+        barraJ: {
+            width: '100%',
+            height: '100%',
+        },
+        barraM: {
+            width: '100%',
+            height: '100%',
+        },
+        logs: []
+
     },
     computed: {
 
     },
     methods: {
-        alearorio(max, min){
+        aleartorio(max, min){
             // Gera um nr aleatório para servir para os calculos de ataque
             return Math.random() * (max - min) + min
         },
+
+        verificaVida(){
+            if(this.vidaJ < 0) {
+                this.vidaJ = 0
+            }
+            if(this.vidaM < 0) {
+                this.vidaM = 0
+            }
+        },
+        verificaTermino(){
+            if(this.vidaJ == 0 || this.vidaM == 0) {
+                return true
+            }
+        },
         Atacar(){
-            let ataqueJ = Math.trunc(this.alearorio(10,5))
-            let ataqueM = Math.trunc(this.alearorio(10,5))
+            let ataqueJ = Math.trunc(this.aleartorio(10,1))
+            let ataqueM = Math.trunc(this.aleartorio(10,1))
             this.vidaJ -= ataqueM
             this.vidaM -= ataqueJ
-            alert(`Ataque Monstro: ${ataqueM}. Ataque Jogador: ${ataqueJ}. `)
+            this.registraLog(`O JOGADOR atingio o mostro com ${ataqueJ}`)
+            if(this.vidaM > 0){
+                this.registraLog(`O MONSTRO atingio o JOGADOR com ${ataqueM}`)
+            }
+            this.verificaVida()
         },
         especial(){
+            let ataqueJ = Math.trunc(this.aleartorio(20,1))
+            let ataqueM = Math.trunc(this.aleartorio(10,1))
+            this.vidaJ -= ataqueM
+            this.vidaM -= ataqueJ
+            this.registraLog(`O JOGADOR atingio o mostro com ${ataqueJ}`)
+            if(this.vidaM > 0){
+                this.registraLog(`O MONSTRO atingio o JOGADOR com ${ataqueM}`)
+            }
+            this.verificaVida()
             
         },
         curar(){
-            let cura =  Math.trunc(this.alearorio(10,5))
-            let ataque = Math.trunc(this.alearorio(10,5))
+            let cura =  Math.trunc(this.aleartorio(10,1))
+            let ataque = Math.trunc(this.aleartorio(10,1))
             this.vidaJ += cura
             this.vidaJ -= ataque
-            alert(`Cura de: ${cura}. Ataque Monstro: ${ataque}. `)
-
+            this.verificaVida()
         },
-        desistir(){
+        iniciarJogo(){
             this.vidaJ = 100
             this.vidaM = 100
             this.starter = !this.starter
+            this.logs = []
         },
+        registraLog(text){
+            this.logs.unshift(text)
+        }
 
     },
     watch: {
-
+ 
     }
   });
