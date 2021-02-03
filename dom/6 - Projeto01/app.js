188c8fbb098758a96360c117544941d1f0c6cc07
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2021-02-03 11:35:18 -0300

    Primeiras codificações do jogo

--- dom/6 - Projeto01/app.js
+++ dom/6 - Projeto01/app.js
@@ -3,24 +3,42 @@ new Vue({
     data: {
         vidaJ: 100,
         vidaM: 100,
+        starter: false,
+    },
+    computed: {
+
     },
     methods: {
+        alearorio(max, min){
+            // Gera um nr aleatório para servir para os calculos de ataque
+            return Math.random() * (max - min) + min
+        },
         Atacar(){
-            this.vidaJ -= 9
-            this.vidaM -= 10
+            let ataqueJ = Math.trunc(this.alearorio(10,5))
+            let ataqueM = Math.trunc(this.alearorio(10,5))
+            this.vidaJ -= ataqueM
+            this.vidaM -= ataqueJ
+            alert(`Ataque Monstro: ${ataqueM}. Ataque Jogador: ${ataqueJ}. `)
         },
         especial(){
-
+            
         },
         curar(){
-            this.vidaJ += 3
-            this.vidaM -= 5
+            let cura =  Math.trunc(this.alearorio(10,5))
+            let ataque = Math.trunc(this.alearorio(10,5))
+            this.vidaJ += cura
+            this.vidaJ -= ataque
+            alert(`Cura de: ${cura}. Ataque Monstro: ${ataque}. `)
 
         },
         desistir(){
             this.vidaJ = 100
             this.vidaM = 100
+            this.starter = !this.starter
         },
+
+    },
+    watch: {
 
     }
   });
