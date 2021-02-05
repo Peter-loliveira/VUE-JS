099431e9e470a732a5883639048077b492c0a47f
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2021-02-05 07:51:43 -0300

    Ajustes

--- dom/6 - Projeto01/app.js
+++ dom/6 - Projeto01/app.js
@@ -3,6 +3,7 @@ new Vue({
     data: {
         vidaJ: 100,
         vidaM: 100,
+        ataqueEspecial: 0,
         starter: false,
         porcentagemJ: '',
         porcentagemM: '',
@@ -15,17 +16,14 @@ new Vue({
             height: '100%',
         },
         logs: []
-
     },
     computed: {
-
     },
     methods: {
         aleartorio(max, min){
             // Gera um nr aleatório para servir para os calculos de ataque
             return Math.random() * (max - min) + min
         },
-
         verificaVida(){
             if(this.vidaJ < 0) {
                 this.vidaJ = 0
@@ -44,14 +42,26 @@ new Vue({
             let ataqueM = Math.trunc(this.aleartorio(10,1))
             this.vidaJ -= ataqueM
             this.vidaM -= ataqueJ
-            this.registraLog(`O JOGADOR atingio o mostro com ${ataqueJ} pontos`, 'logJ')
+            if(ataqueJ == 1) {
+                this.registraLog(`O JOGADOR atingio o mostro com ${ataqueJ} ponto`, 'logJ')
+            }else {
+                this.registraLog(`O JOGADOR atingio o mostro com ${ataqueJ} pontos`, 'logJ')
+            }
             if(this.vidaM > 0){
-                this.registraLog(`O MONSTRO atingio o JOGADOR com ${ataqueM} pontos`, 'logM')
+                if(ataqueM == 1) {
+                    this.registraLog(`O MONSTRO atingio o JOGADOR com ${ataqueM} ponto`, 'logM')
+                } else {
+                    this.registraLog(`O MONSTRO atingio o JOGADOR com ${ataqueM} pontos`, 'logM')
+                }
+            }
+            if(this.ataqueEspecial < 100 ) {
+                this.ataqueEspecial += 25
+                console.log(this.ataqueEspecial);
             }
             this.verificaVida()
         },
         especial(){
-            let ataqueJ = Math.trunc(this.aleartorio(20,1))
+            let ataqueJ = Math.trunc(this.aleartorio(15,7))
             let ataqueM = Math.trunc(this.aleartorio(10,1))
             this.vidaJ -= ataqueM
             this.vidaM -= ataqueJ
@@ -59,6 +69,7 @@ new Vue({
             if(this.vidaM > 0){
                 this.registraLog(`O MONSTRO atingio o JOGADOR com ${ataqueM} pontos`, 'logM')
             }
+            this.ataqueEspecial = 0
             this.verificaVida()
             
         },
@@ -67,7 +78,16 @@ new Vue({
             let ataque = Math.trunc(this.aleartorio(10,1))
             this.vidaJ += cura
             this.vidaJ -= ataque
-            this.registraLog(`O Jogador se curou um total de ${cura} pontos`, 'logJ')
+            let totalCura = cura - ataque
+            if (totalCura < 0) {
+                this.registraLog(`O Jogador se curou ${cura} pontos, mas recebeu um ataque de ${ataque} pontos.`, 'logJ')
+            } else {
+                if (totalCura == 1) {
+                    this.registraLog(`O Jogador se curou um total de ${totalCura} ponto`, 'logJ')
+                }else {
+                    this.registraLog(`O Jogador se curou um total de ${totalCura} pontos`, 'logJ')
+                }
+            }
             this.verificaVida()
         },
         iniciarJogo(){
@@ -75,13 +95,12 @@ new Vue({
             this.vidaM = 100
             this.starter = !this.starter
             this.logs = []
+            this.ataqueEspecial = 0
         },
         registraLog(text, css){
             this.logs.unshift({text, css})
         }
-
     },
     watch: {
- 
     }
   });
