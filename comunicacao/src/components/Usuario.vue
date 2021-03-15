2f2bc8479d497de01222e95f58751f4676a295f5
Peter Lange <peter.loliveira@gmail.com>
2021-03-15 10:42:48 -0300

    Comunicação ENTRE componentes

---
+++ comunicacao/src/components/Usuario.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <h1>Componente Usuário</h1>
+    <p>Esse é um componente muito legal!</p>
+    <p>O nome original é <strong> {{ nomeLocal }} </strong> </p>
+    <button @click="alterarDados">Alterar Dados</button>
+    <hr />
+    <div class="componentes">
+      <!-- Ambas as formas são equivalentes: -->
+      <!-- <AppUsuarioInfo v-bind:nomeInfo = 'nomeLocal' v-bind:idadeInfo = 'idadeLocal * 2'/> -->
+      <AppUsuarioInfo 
+          :nomeInfo = 'nomeLocal' 
+          :idadeInfo = 'idadeLocal' 
+          @nomeMudou = 'nomeLocal = $event.nome, idadeLocal = $event.idadeInfo'
+          :reiniciaFn = 'reiniciarNome'
+          />
+      <AppUsuarioEditar />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
+
+export default {
+  components: { AppUsuarioInfo, AppUsuarioEditar },
+  data() {
+    return {
+      nomeLocal: "Peter",
+      idadeLocal: 45,
+    };
+  },
+  methods: {
+    alterarDados() {
+      this.nomeLocal += "!";
+      this.idadeLocal++;
+    },
+    reiniciarNome(){
+      this.nomeLocal = 'Peter'
+      this.idadeLocal = 45
+    }
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
+
+.container hr {
+  margin: 20px 10px;
+}
+
+.componentes {
+  display: flex;
+}
+
+.componentes > * {
+  margin: 10px;
+}
+
+button {
+  background-color: #ec485f;
+  color: #fff;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  border: 1px solid yellow;
+  outline: none;
+}
+</style>
---
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong> {{ inverterNome() }} </strong> tem <strong> {{ idadeInfo }} </strong> anos.</p>
+        <button @click = 'reiniciaNome'>Reinicar Dados</button>
+        <button @click = 'reiniciaFn'>Reinicar Por Função (Callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: ['nomeInfo', 'idadeInfo'], Notação Normal e simples
+
+    // NOtação com checagem de tipo e obrigatoriedade de preencimento
+    props: {
+        nomeInfo: {
+            type: String, //Checagem de tipos
+            require: true, // Obrigatoriedade de preenchimento
+            default: 'EMON MES' //Caso não seja enviado, assume esse valor como padrão
+            // Também é possivel usar uma função como valor default :
+            // default: function() {
+            //     return Array(10).fill(0).join(',')
+            // }
+        },
+        idadeInfo: Number,
+        reiniciaFn: Function
+    },
+    methods: {
+        inverterNome() {
+            return this.nomeInfo.split('').reverse().join('')
+        },
+        reiniciaNome() {
+            // this.nomeInfo = 'Peter'
+            this.$emit('nomeMudou', {
+                nome: 'peter',
+                idadeInfo: 45})
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+    button {
+        background-color: aqua;
+        border: 0px;
+        outline: none;
+    }
+
+</style>
